04cb669c48bf9e3447c4a88daa59a59e5c3ec836
MueanNimrodVipinosa <115630458+MueanNimrodVipinosa@users.noreply.github.com>
2022-12-03 12:06:33 +0800

    START

---
+++ Vipinosa_friend_list.js
@@ -0,0 +1,46 @@
+[
+    {
+        "First_Name" : "MUEAN NEMROD",
+        "Last_Name" : "VIPINOSA",
+        "Age" : "21",
+        "Address" : "BAROTAC",
+        "Contact_Number" : "09065816492"
+    },
+  
+    {
+        "Friends List" : [
+          {
+              "Name": "al jude",
+              "Age": "21",
+              "Address": "Pavia",
+              "Likes" :"SINGING",
+              "Dislikes" : "Filipino Subject", 
+              "Feeling" : "Tired"
+          },
+          {
+            "Name": "ronz",
+            "Age": "21",
+            "Address": "Antique",
+            "Likes" :"SINGING",
+            "Dislikes" : "Filipino Subject", 
+            "Feeling" : "Tired"
+        },
+          {
+            "Name": "rancel",
+            "Age": "22",
+            "Address": "Pavia",
+            "Likes" :"Dancing ",
+            "Dislikes" : " FIlipino", 
+            "Feeling" : " Tired"
+          },
+          {
+            "Name": "Christian",
+            "Age": "22",
+            "Address": "Guimaras",
+            "Likes" :"Playing Video Games",
+            "Dislikes" : "Math", 
+            "Feeling" : "Tired"
+          }]
+      }
+ 
+]
